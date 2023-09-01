441eaa252989d61c859d03fe5c9ffcbeac862589
Jeff Allen <jeffallen89@gmail.com>
2023-09-01 09:44:39 -0400

    svelte page setup

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/violations/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/violations/+page.svelte";
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    import '../assets/global.css';
+
+    var text = "some text"
+
+</script>
+
+
+<main>
+
+    <div id="content">
+
+        {text}
+
+    </div>
+
+</main>
+
+
+
+<style>   
+
+    #content {
+        height: 100vh;
+        background-color: #1E3765;
+        color: #F6F6F5;
+    }
+
+</style>
