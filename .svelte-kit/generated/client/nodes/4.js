d8abb94f9bca614985da4ca9b6fcfda1a1702afb
Jeff Allen <jeffallen89@gmail.com>
2023-11-02 13:40:25 -0400

    force removing .svelte-kit, should be in the gitignore, now it is

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/violations/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/violations/+page.svelte";
