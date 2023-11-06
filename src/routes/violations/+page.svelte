2093a0d52b7e2f7a1a748e435960f2d250633a47
Jeff Allen <jeffallen89@gmail.com>
2023-11-06 08:36:14 -0500

    some design tweaks, add in some text

--- src/routes/violations/+page.svelte
+++ src/routes/violations/+page.svelte
@@ -1,6 +1,6 @@
 <script>
+
     import '../../assets/global.css';
-    import * as d3 from "d3";
     import ViolationsChart2020s from "../../lib/ViolationsChart2020s.svelte";
     import ViolationsChart2019 from "../../lib/ViolationsChart2019.svelte";
     import TotalViolations2019Chart from "../../lib/TotalViolations2019Chart.svelte";
@@ -11,12 +11,15 @@
 
 <main>
 
-
+    <div class="text">
+        <p>
+            It has been about six years since the City of Toronto altered traffic restrictions on King Street to give priority to public transit over private vehicles. Since the initiation of the project, cars have not been allowed to travel straight or make left turns at many intersections, reducing through traffic volumes.
+        </p>
+    </div>
 
     <h2>Total Violations, 2019</h2>
     
     <TotalViolations2019Chart />
-
 
     
     <h2>Intersection Violations, 2019</h2>
@@ -58,7 +61,6 @@
         intersectionID="20"
     />
 
-
     <h2>Intersection Violations, 2020-2023</h2>
 
     <ViolationsChart2020s
@@ -81,3 +83,18 @@
     />
 
 </main>
+
+
+
+
+<style>
+
+    .text {
+        background-color: red;
+        max-width: 600px;
+        margin: 0 auto;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+
+</style>
